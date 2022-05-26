c628d853ae9d3f8a4d700eb28fa117e529fad216
milfordestayschiller <103213100+milfordestayschiller@users.noreply.github.com>
2022-05-25 20:06:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,402 +1,167 @@
-//Se identifican los ids y la clase square
+// Declaracion de variables
+let colors = generateRandomColors()
+let cuadrados = document.querySelectorAll(".square")
+let message = document.querySelector("#message")
+let reset = document.querySelector("#reset")
+let easy = document.querySelector("#easy")
+let resetNew = document.querySelector("#reset2")
+//Funcion de reciclaje (recomendacion de la pauta)
+function requestFunction(params) {
+   for (let i = 0; i < cuadrados.length; i++) {
+      cuadrados[i].style.background = colors[i]
+      console.log(cuadrados[i])
+   }
 
-var message = document.getElementById("message")
-var titulo = document.getElementById("titulo")
-var square = document.querySelectorAll(".square");
-var valorTexto = document.getElementById("colorDisplay")
-var x = Math.floor(Math.random() * 6)
-//Se asigna a la variable randomColor
-valorTexto.textContent = randomColor()
+}
+requestFunction()
+let rgb = document.querySelector("#rgb")
+//Asignacion a PickedColor
+let pickedColor = pickColor()
+//Asignacion al titulo de PickedColor
+rgb.textContent = pickedColor
+//Comparacion modo Hard
+function comparative(params) {
 
-
-//Funcion que sirve para recorrer los cuadrados de la posicion 0 a 5 , ademas utiliza la funcion randomColor para hacer las comparaciones correspondientes y que el color aleatorio RGB del h1 sea coincidente con uno de los cuadrados, para posteriormente convierta a todos los demas cuadrados en el color que se selecciono como ganador.
-
-
-function randomGlobal(valor) {
-   for (i = 0; i < square.length; i++) {
-
-      if (square[i].style.backgroundColor = randomColor()) {
-
-         square[x].style.backgroundColor = valorTexto.textContent;
+   for (let i = 0; i < cuadrados.length; i++) {
+      cuadrados[i].addEventListener('click', function (event) {
+         let valorClick = cuadrados[i]
+         if (valorClick.style.backgroundColor == pickedColor) {
+            console.log("gano")
+            message.textContent = "Correcto"
+            rgb.style = `background: ${valorClick.style.backgroundColor} `
+            changeColors(valorClick.style.backgroundColor)
+            reset.textContent = "Play Again?"
+            
+         }
+         else {
+            console.log("perdio")
+            valorClick.style = "background-color:#232323; transition: 0.5s;"
+            message.textContent = "Intentelo Nuevamente"
+         }
       }
 
+      )
    }
-   //Se invoca a la funcion factory donde se hacen las comparaciones de los clicks  de los cuadrados y si es el ganador esta funcion se puede ver al final
-   factory2()
+}
+//Comparacion modo Easy
+function comparative2(params) {
+
+   for (let i = 0; i < 3; i++) {
+      cuadrados[i].addEventListener('click', function (event) {
+         let valorClick = cuadrados[i]
+         if (valorClick.style.backgroundColor == pickedColor) {
+            message.textContent = "Correcto"
+            rgb.style = `background: ${valorClick.style.backgroundColor} `
+            changeColors(valorClick.style.backgroundColor)
+            resetNew.textContent = "Play Again"
+         }
+         else {
+            valorClick.style = "background-color:#232323; transition: 0.5s;"
+            message.textContent = "Intentelo Nuevamente"
+         }
+      }
+
+      )
+   }
 }
 
-randomGlobal()
+//Cambio de colores
+function changeColors(color) {
+   for (let i = 0; i < cuadrados.length; i++) {
+      cuadrados[i].style.backgroundColor = color
+   }
+}
 
-// Funcion para dar el color random en el rgb  en h1
+changeColors()
+//Funcion random para PickedcOLOR
+function pickColor(params) {
+   let rand = Math.floor(Math.random() * colors.length)
+   let arreglo = colors[rand]
+   return arreglo
+}
+pickColor()
+
+//Asigna el color rgb en el arrego y es utilizada en generateRandomcOLORS
 function randomColor() {
    var color1 = Math.floor(Math.random() * 256)
    var color2 = Math.floor(Math.random() * 256)
    var color3 = Math.floor(Math.random() * 256)
    return "rgb(" + color1 + ", " + color2 + ", " + color3 + ")"
+
 }
 
 randomColor()
+//Genera los rgv y los agrega a cada indice del arreglo en 6 elementos, luego se asiga a la variable colors solicitado en la pauta, esto genera los colores para hard
+function generateRandomColors() {
+   let nuevosColores = []
 
-//Clase easy seleccionada 
-var boton = document.querySelector(".easy")
-var hidden = document.querySelector("#ultimosTres")
-var primerosTres = document.querySelector("#primerosTres")
+   for (var i = 0; i < 6; i++) {
+      nuevosColores[i] = randomColor()
+   }
+   return nuevosColores
+}
+//Genera los colores para Easy
+function generateRandomColors2() {
+   let nuevosColores = []
 
-//Funcion asociada al evento click de la clase easy, se crean y eliminan elementos
-
-boton.addEventListener("click", function (params) {
-   playAgain.style = "visibility: hidden !important"
-   playAgain2.style = "visibility: hidden"
-   titulo.style = "background: #4682b4 !important;"
-   reset.style = "visibility: hidden";
-   message.textContent = "";
-   reset2.style = "visibility: visible !important"
-   titulo.style = "background: #232323 !important;";
-   hidden.style = "visibility:hidden";
-   primerosTres.style = "visibility: visible !important"
-
-
-   var z = Math.floor(Math.random() * 3)
-
-   valorTexto.textContent = randomColor()
-
-   for (i = 0; i < square.length; i++) {
-
-      if (square[i].style.backgroundColor = randomColor()) {
-
-         square[z].style.backgroundColor = valorTexto.textContent;
-         primerosTres.style = "visibility: visible !important"
+   for (var i = 0; i < 3; i++) {
+      nuevosColores[i] = randomColor()
+   }
+   return nuevosColores
+}
+//Funcion refactorizada para su utilizacion en el resetear
+function reset2(params) {
+   for (var i = 0; i < cuadrados.length; i++) {
+      if (cuadrados[i]) {
+         cuadrados[i].style.background = colors[i]
+         cuadrados[i].style.display = "block"
+      } else {
+         cuadrados[i].style.display = "none"
       }
    }
-   factory1()
-})
-
-
-var reset = document.querySelector("#reset")
-
-//Reset que permite la generacion deL BOTON Nuevos colores en fase Hard
-
-reset.addEventListener("click", function (params) {
-   playAgain2.style = "visibility: hidden"
-   valorTexto.textContent = randomColor()
-   var reseteo = Math.floor(Math.random() * 6)
-   for (i = 0; i < square.length; i++) {
-
-      if (square[i].style.backgroundColor = randomColor()) {
-
-         square[reseteo].style.backgroundColor = valorTexto.textContent;
-
-      }
-
-   }
-   factory2()
-})
-//Reset que permite la generacion del BOTON nuevos colores en fase Easy
-
-var reset2 = document.querySelector("#reset2")
-
-reset2.addEventListener("click", function () {
-   playAgain2.style = "visibility: hidden"
-   message.textContent = "";
-   titulo.style = "background: #232323"
-
-   valorTexto.textContent = randomColor()
-
-   var z = Math.floor(Math.random() * 3)
-
-
-
-   for (i = 0; i < square.length; i++) {
-
-      if (square[i].style.backgroundColor = randomColor()) {
-
-         square[z].style.backgroundColor = valorTexto.textContent;
-         primerosTres.style = "visibility: visible !important"
-      }
-
-
-   }
-   // Se hace recorrido del div square de la posicion 0 a 2
-   factory1()
-
-})
-
-//playAgain permite volver a jugar en modo Hard se hace reload en esta sección y redefine los colores de los cuadrados
-
-var playAgain = document.querySelector("#playAgain")
-playAgain.addEventListener("click", function (event) {
-
-   factory2()
-   playAgain.style = "visibility: hidden"
-
-   primerosTres.style = "visibility: visible !important"
-
-   var z = Math.floor(Math.random() * 6)
-
-   valorTexto.textContent = randomColor()
-
-   for (i = 0; i < square.length; i++) {
-
-      if (square[i].style.backgroundColor = randomColor()) {
-
-         square[z].style.backgroundColor = valorTexto.textContent;
-         primerosTres.style = "visibility: visible !important"
-      }
-   }
-
-   factory1()
-})
-
-//PlayAgain2 correspondiente a boton easy
-
-var playAgain2 = document.querySelector("#playAgain2")
-playAgain2.addEventListener("click", function (params) {
-   playAgain2.style = "visibility: hidden !important"
-   reset.style = "visibility: hidden";
-   message.textContent = "";
-   reset2.style = "visibility: visible !important"
-   titulo.style = "background: #232323 !important;";
-
-   primerosTres.style = "visibility: visible !important"
-
-   var z = Math.floor(Math.random() * 3)
-
-   valorTexto.textContent = randomColor()
-
-   for (i = 0; i < square.length; i++) {
-
-      if (square[i].style.backgroundColor = randomColor()) {
-
-         square[z].style.backgroundColor = valorTexto.textContent;
-         primerosTres.style = "visibility: visible !important"
-      }
-
-
-   }
-
-   factory1()
-
-})
-
-//Factorizacion de Codigo factory1 y factory2 , estas funciones se reinvocan en otras funciones necesarias
-//factory1 para la Seccion Easy
-function factory1(params) {
-   var clickedColor0 = square[0].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-
-      if (valorTexto.textContent == square[0].style.backgroundColor) {
-         message.textContent = "Ganaste"
-         reset2.style = "visibility: hidden !important"
-         titulo.style.backgroundColor = valorTexto.textContent
-         playAgain2.style = "visibility: visible !important"
-         playAgain.style = "visibility: hidden !important"
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent
-         }
-      }
-      else {
-         message.textContent = "perdiste"
-         square[0].style = "background-color:#232323;"
-      }
-
+   rgb.style.background = "steelblue"
+ 
+}
+//Resetea los colores, el titulo, se cambian los colores
+function resetear() {
+   comparative()
+   reset.addEventListener("click", function (params) {
+       colors = generateRandomColors()
+      pickedColor = pickColor()
+      rgb.textContent = pickedColor
+      reset2()
    })
-   var clickedColor0 = square[1].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-      if (valorTexto.textContent == square[1].style.backgroundColor) {
-
-         message.textContent = "Ganaste"
-         reset2.style = "visibility: hidden !important"
-         titulo.style.backgroundColor = valorTexto.textContent
-         playAgain2.style = "visibility: visible !important"
-         playAgain.style = "visibility: hidden !important"
-
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent;
-
-         }
-      }
-      else {
-         message.textContent = "perdiste"
-         square[1].style = "background-color:#232323;"
-      }
-
-
-   })
-   var clickedColor0 = square[2].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-      if (valorTexto.textContent == square[2].style.backgroundColor) {
-
-         message.textContent = "Ganaste"
-         reset2.style = "visibility: hidden !important"
-         titulo.style.backgroundColor = valorTexto.textContent
-         playAgain2.style = "visibility: visible !important"
-         playAgain.style = "visibility: hidden !important"
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent
-         }
-
-      }
-      else {
-         message.textContent = "perdiste"
-         square[2].style = "background-color:#232323;"
-      }
-
-   })
-
-   return clickedColor0
-
 }
 
-factory1()
+resetear()
 
-//Factory2 para la seccion Hard, se captura el click por cada indice , si el valor del random es igual al indice del cuadrado con su respectivo color (rgb) entonces gana de lo contrario pierde.
-//Esta funcion se invoca en randomGlobal
-
-function factory2(params) {
-
-
-   var clickedColor0 = square[0].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-      if (valorTexto.textContent == square[0].style.backgroundColor) {
-         playAgain.style = "visibility: visible"
-         playAgain2.style = "visibility: hidden !important"
-         message.textContent = "Ganaste"
-         reset.style = "visibility: hidden !important"
-
-         titulo.style.backgroundColor = valorTexto.textContent
-
-
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent
-         }
-
-      }
-      else {
-         message.textContent = "perdiste"
-         square[0].style = "background-color:#232323;"
-      }
-
+function resetear2() {
+   comparative()
+   resetNew.addEventListener("click", function (params) {
+       colors = generateRandomColors2()
+      pickedColor = pickColor()
+      rgb.textContent = pickedColor
+      reset2()
    })
-   var clickedColor0 = square[1].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-      if (valorTexto.textContent == square[1].style.backgroundColor) {
-         playAgain.style = "visibility: visible"
-         playAgain2.style = "visibility: hidden !important"
-         message.textContent = "Ganaste"
-         reset.style = "visibility: hidden !important"
-
-         titulo.style.backgroundColor = valorTexto.textContent
-
-
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent
-         }
-
-      }
-      else {
-         message.textContent = "perdiste"
-         square[1].style = "background-color:#232323;"
-      }
-
-   })
-   var clickedColor0 = square[2].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-      if (valorTexto.textContent == square[2].style.backgroundColor) {
-         playAgain.style = "visibility: visible"
-         playAgain2.style = "visibility: hidden !important"
-         message.textContent = "Ganaste"
-         reset.style = "visibility: hidden !important"
-
-         titulo.style.backgroundColor = valorTexto.textContent
-
-
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent
-         }
-
-      }
-      else {
-         message.textContent = "perdiste"
-         square[2].style = "background-color:#232323;"
-      }
-
-   })
-   var clickedColor0 = square[3].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-      if (valorTexto.textContent == square[3].style.backgroundColor) {
-         playAgain.style = "visibility: visible"
-         playAgain2.style = "visibility: hidden !important"
-         message.textContent = "Ganaste"
-         reset.style = "visibility: hidden !important"
-
-         titulo.style.backgroundColor = valorTexto.textContent
-
-
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent
-         }
-
-      }
-      else {
-         message.textContent = "perdiste"
-         square[3].style = "background-color:#232323;"
-      }
-
-   })
-   var clickedColor0 = square[4].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-      if (valorTexto.textContent == square[4].style.backgroundColor) {
-         playAgain.style = "visibility: visible"
-         playAgain2.style = "visibility: hidden !important"
-         message.textContent = "Ganaste"
-         reset.style = "visibility: hidden !important"
-
-         titulo.style.backgroundColor = valorTexto.textContent
-
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent
-         }
-
-      }
-      else {
-         message.textContent = "perdiste"
-         square[4].style = "background-color:#232323;"
-      }
-
-   })
-   var clickedColor0 = square[5].addEventListener("click", function () {
-      var message = document.getElementById("message")
-
-      if (valorTexto.textContent == square[5].style.backgroundColor) {
-         playAgain.style = "visibility: visible"
-         playAgain2.style = "visibility: hidden !important"
-         reset.style = "visibility: hidden !important"
-         message.textContent = "Ganaste"
-
-         titulo.style.backgroundColor = valorTexto.textContent
-
-         for (i = 0; i < square.length; i++) {
-            square[i].style.backgroundColor = valorTexto.textContent
-         }
-
-      }
-      else {
-         message.textContent = "perdiste"
-         square[5].style = "background-color:#232323;"
-      }
-
-   })
-   return clickedColor0
-
-
 }
 
-factory2()
+resetear2()
+//Se ocultan los cuadrados al hacer click en easy
+let ocultar = document.getElementById("ocultar")
+//Se invoca a la funcion generateRandomColors2 para que haga un random en las primeras 3 posiciones del arreglo
+easy.addEventListener("click", function (params) {
+   comparative2()
+   colors = generateRandomColors2()
+   pickedColor = pickColor()
+   rgb.textContent = pickedColor
+   reset.style = "visibility: hidden"
+   resetNew.style = "visbility: visible"
+   reset2()
+   for (let i = 0; i < cuadrados.length - 3; i++) {
+      if (cuadrados[i]) {
+         ocultar.style = "visibility: hidden"
+         cuadrados[i].style.background = colors[i]
+
+      }
+     }
+})
